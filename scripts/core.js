0d14586b6143c233d74f434138b77d5bd2b6287f
Nick Pearson <nicholas.pearson4@btinternet.com>
2018-03-12 02:22:07 +0000

    Implemented search box, some styling work

--- scripts/core.js
+++ scripts/core.js
@@ -28,3 +28,58 @@ function LoadAndRenderTune(filepath, div)
 
   xhr.send();
 }
+
+function MatchesFilter(inText, filter)
+{
+  filter = filter.trim();
+  if(filter.length == 0) return true;
+
+  var text = inText.toLowerCase();
+  var filters = filter.toLowerCase().split(" ");
+
+  var result = false;
+
+  filters.forEach(function(fi) {
+    if(text.search(fi) != -1)
+    {
+      result = true;
+      return;
+    }
+  });
+
+  return result;
+}
+
+function HighlightText(inText, filter)
+{
+  filter = filter.trim();
+  if(filter.length == 0) return inText;
+
+  var filters = filter.toLowerCase().split(" ");
+
+  return HighlightText_recursive(inText, filters);
+}
+
+function HighlightText_recursive(inText, filters)
+{
+  var text = inText.toLowerCase();
+  if(text.length == 0) return "";
+
+  var lowestIdx = -1;
+  var lowestMatch = "";
+
+  filters.forEach(function(fi) {
+    var match = text.search(fi);
+
+    if(match != -1 &&
+        (lowestMatch == "" || match < lowestMatch))
+    {
+      lowestIdx = match;
+      lowestMatch = fi;
+    }
+  });
+
+  if(lowestIdx == -1) return inText;
+
+  return inText.substr(0,lowestIdx) + "<span class=\"highlighted-text\">" + inText.substr(lowestIdx, lowestMatch.length) + "</span>" + HighlightText_recursive(inText.substr(lowestIdx + lowestMatch.length), filters);
+}
