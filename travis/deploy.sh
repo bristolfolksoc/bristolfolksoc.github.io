19e370f8e92093e79e1fba009a20932e65574cc8
Nick Pearson <nicholas.pearson4@btinternet.com>
2019-05-21 11:44:06 +0100

    wrong directory

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,6 +3,7 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false"  ]; then
 
   if [ "$TRAVIS_BRANCH" == "development" ]; then
+    cd ..
     git config --global user.email "deploy@travis-ci.org"
     git config --global user.name "Deployment Bot"
     rm .gitignore
@@ -12,6 +13,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false"  ]; then
     git commit -m "Build $TRAVIS_BUILD_NUMBER"
     git remote add origin-pages https://${GITHUB_TOKEN}@github.com/bristolfolksoc/bristolfolksoc.github.io.git > /dev/null 2>&1
     git push --quiet -f origin-pages master
+    cd ..
   else
     echo "Skipping depoyment, deployment only occurs on the development branch";
   fi
